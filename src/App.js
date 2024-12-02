58d8a8023b0c6d42c72181a85349717df4920b1f
donia-essam <s-donia.soliman@zewailcity.edu.eg>
2024-12-03 00:26:24 +0200

    front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Registration from "./components/auth/Registration";
+import Login from './components/auth/Login';
+import FavoritesPage from './components/customer/FavoritesPage'; // Import the FavoritesPage component
+import SearchPage from './components/search/SearchPage'; // Import the SearchPage component
+import PortfolioManager from './components/artisan/PortfolioManager'; // Import the PortfolioManager component
+import ProfileManagement from './components/profile/ProfileManagement'; // Import the ProfileManagement component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/favorites" element={<FavoritesPage />} /> {/* Add the FavoritesPage route */}
+        <Route path="/search" element={<SearchPage />} /> {/* Add the SearchPage route */}
+        <Route path="/portfolio" element={<PortfolioManager />} /> {/* Add the PortfolioManager route */}
+        <Route path="/profile" element={<ProfileManagement />} /> {/* Add the ProfileManagement route */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/home/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: "#333", color: "#fff", padding: "10px", textAlign: "center" }}>
+      <p>© 2024 جميع الحقوق محفوظة</p>
+    </footer>
+  );
+}
+
+export default Footer;
